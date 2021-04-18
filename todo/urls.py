1998b1cb66866fdefbabe801723b6075b1a5c059
beka97 <beksultan.ulukbekov@company.com>
2021-04-18 13:48:08 +0300

    added todo unmarked

--- todo/urls.py
+++ todo/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
     path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("unmark-todo/<id>/", unmark_todo, name="unmark-todo"),
 ]   + static(settings.STATIC_URL, docyment_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, docyment_root=settings.MEDIA_ROOT)
